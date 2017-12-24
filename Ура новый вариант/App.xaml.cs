751971a91de76806ca4ce45c5cb68ba3ba87b32b
Vladislav <ugu.03@mail.ru>
2017-12-25 02:30:50 +0300

    Последний

--- Ура новый вариант/App.xaml.cs
+++ Ура новый вариант/App.xaml.cs
@@ -13,5 +13,18 @@ namespace Ура_новый_вариант
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+        }
+
+        private static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+
+            System.Diagnostics.EventLog loggggger = new System.Diagnostics.EventLog();
+            loggggger.WriteEntry("Exception was found");
+            System.Environment.Exit(1);
+        }
     }
 }
--- Ура новый вариант/SaveCommand.cs
+++ Ура новый вариант/SaveCommand.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Configuration;
 
 namespace Ура_новый_вариант
 {
@@ -20,23 +21,33 @@ namespace Ура_новый_вариант
 
         public void Execute(object parameter)
         {
-            var apply = parameter as MainWindowsViewModel;
-            if (apply.SelectedCity == null || String.IsNullOrWhiteSpace(apply.SelectedCity.Name) || apply.SelectedStreet == null || String.IsNullOrWhiteSpace(apply.SelectedStreet.Name) ||
-                apply.Surname == null || apply.Name == null || apply.Patronymic == null || apply.Home == null || apply.Home == 0 ||
-                apply.StrK == null || apply.StrK == 0 || apply.Apartament == null || apply.Apartament == 0)
+            try
             {
-                MessageBox.Show("Данные некорректны, проверьте и введите заново");
-            }
-            else
-            {
-                using (SqlConnection connection = new SqlConnection("Server=Demidoff\\SQLEXPRESS;Database=BDVlad;Trusted_Connection=True;"))
+                var apply = parameter as MainWindowsViewModel;
+                if (apply.SelectedCity == null || String.IsNullOrWhiteSpace(apply.SelectedCity.Name) || apply.SelectedStreet == null || String.IsNullOrWhiteSpace(apply.SelectedStreet.Name) ||
+                    apply.Surname == null || apply.Name == null || apply.Patronymic == null || apply.Home == null || apply.Home == 0 ||
+                    apply.StrK == null || apply.StrK == 0 || apply.Apartament == null || apply.Apartament == 0)
                 {
-                    connection.Open();
-                    String query = String.Format("INSERT INTO Table_3(Surname, Name, Patronymic, City, Street, Home, StrK, Apartament) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')", apply.Surname, apply.Name, apply.Patronymic, apply.SelectedCity.Name, apply.SelectedStreet.Name, apply.Home, apply.StrK, apply.Apartament);
-                    SqlCommand cmdSave = new SqlCommand(query, connection);
-                    cmdSave.ExecuteNonQuery();
+                    MessageBox.Show("Данные некорректны, проверьте и введите заново");
                 }
-                MessageBox.Show("Вы успешно зарегистрированы");
+                else
+                {   //REVIEW: Вынести строку подключения в настройки
+                    using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ConnectionString))
+                    {
+
+                        //REVIEW: Если соединение не открывается или команда не проходит - будет исключение
+                        connection.Open();
+                        String query = String.Format("INSERT INTO Table_3(Surname, Name, Patronymic, City, Street, Home, StrK, Apartament) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')", apply.Surname, apply.Name, apply.Patronymic, apply.SelectedCity.Name, apply.SelectedStreet.Name, apply.Home, apply.StrK, apply.Apartament);
+                        SqlCommand cmdSave = new SqlCommand(query, connection);
+                        cmdSave.ExecuteNonQuery();
+                    }
+                    MessageBox.Show("Вы успешно зарегистрированы");
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.EventLog logggger = new System.Diagnostics.EventLog();
+                logggger.WriteEntry($"An error wasfound during sql server command execution: {e.Message}");
             }
         }
     }
